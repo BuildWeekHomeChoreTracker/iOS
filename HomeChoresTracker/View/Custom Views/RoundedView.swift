dd395c306baaf1d5428e40581b1d4a8d1e6b0e22
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-03 15:27:01 -0500

    Added Storyboards and stubbed out the basic UI

---
+++ HomeChoresTracker/View/Custom Views/RoundedView.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedView.swift
+//  HomeChoresTracker
+//
+//  Created by Chad Rutherford on 2/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    private func sharedInit() {
+        layer.cornerRadius = 15.0
+        clipsToBounds = true
+    }
+}
