379cf0362440b8e91521480e42b1703b8531c829
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-03 19:06:17 -0800

    Created User to represent login with username and password. Created helper method in UserController to create a request and encode a user in one step

---
+++ HomeChoresTracker/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  HomeChoresTracker
+//
+//  Created by Kenny on 2/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
