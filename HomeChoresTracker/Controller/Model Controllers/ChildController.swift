0f79df37aac86f86e9395addc62fb11e34a9abe5
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-02-05 17:14:52 -0800

    updated PUT request

--- HomeChoresTracker/Controller/Model Controllers/ChildController.swift
+++ HomeChoresTracker/Controller/Model Controllers/ChildController.swift
@@ -158,7 +158,7 @@ class ChildController {
         }
         
         let updateChoreURL = updateURL?.appendingPathComponent(String(chore.id))
-        guard var request = NetworkService.createRequest(url: updateChoreURL, method: .put) else {
+        guard var request = NetworkService.createRequest(url: updateChoreURL, method: .put, headerType: .contentType, headerValue: .json) else {
             let error = NSError(domain: "ChildController.updateChore: \(String(describing: chore.title)).requestError", code: NetworkService.NetworkError.badRequest.rawValue)
             complete(error)
             return
